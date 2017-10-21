ca6fee125deed0d1d84a89d1464fc56f2d05dd19
Ismail Monsurat <monsurat.ismail@epicacademy.org>
2017-10-21 16:46:17 +0000

    todo list

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,9 @@
 class TodoController< ApplicationController
     def index
     end
+    
+    def show
+        @todo_desciption="make the curriculum"
+        @todo_pomodoro_estimate= 5
+    end
 end
