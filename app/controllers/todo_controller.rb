1046be7017a609b6fabd1db662c5eaa9e8a905a2
Ismail Monsurat <monsurat.ismail@epicacademy.org>
2017-10-22 16:44:56 +0000

    add new todo view

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -5,6 +5,14 @@ class TodoController< ApplicationController
     def new
     end
     
+    def create
+       t= Todo.new
+       t.description= params[:description]
+       t.pomodoro_estimate= params[:pomodoro_estimate]
+       t.complete= false 
+       t.save
+       redirect_to "/todo/show/#{t.id}"
+    end
     
     def show
         @my_todo = Todo.find_by_id(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get "todo/index",to:"todo#index"
   get "todo/show/:id",to:"todo#show"
   get "todo/new",to:"todo#new"
+  post "todo/create",to:"todo#create"
 end
+
 
 
 
